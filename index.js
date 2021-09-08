8f0646969cff92d24e2d6f7fbb9b1d2c95313805
Philipp Binder <binder.philipp10@gmail.com>
2021-09-08 15:38:33 +0200

    Add .gitignore

---
+++ index.js
@@ -0,0 +1,39 @@
+/*
+    ESLINT-VERIFICATION DID NOT WORK
+
+const fs = require('fs');
+
+const num = 1;
+let a = 'str';
+console.log(fs, a.substr(0));
+
+function b(num) {
+  if (true) {
+    console.log(num);
+
+    // JSX does not throw an error when React is
+    // not in scope because of the new JSX transform
+    // https://github.com/yannickcr/eslint-plugin-react/issues/2440#issuecomment-683433266
+    const jsx = <div />;
+    console.log(jsx);
+  }
+}
+
+b();
+
+c();
+
+const d = { e: { f: 1 } };
+
+console.log(d?.e?.f);
+
+function Component() {}
+Component();
+
+const OtherComponent = (x, y) => {};
+
+const PascalCaseVar = 1;
+const snake_case_var = 1;
+
+OtherComponent(PascalCaseVar, snake_case_var);
+*/
