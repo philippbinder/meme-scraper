6f6ce2a1bbf72456589eb3578098e8ede181370e
Philipp Binder <binder.philipp10@gmail.com>
2021-09-09 23:13:56 +0200

    Addd updated the code?

--- index.js
+++ index.js
@@ -17,13 +17,14 @@ installed dependancies:
   - Node Fetch https://www.npmjs.com/package/node-fetch - for fechting the images from the meme page
   - ESM https://reactgo.com/node-es6-imports/ - for making Node Fetch work
   - maybe instead https://www.npmjs.com/package/esm
+  - Cheerio https://www.npmjs.com/package/cheerio
 
 miscellaneous:
   https://www.geeksforgeeks.org/node-js-fs-mkdir-method/ - shows me the code I need to write for dir on other PC's who use my file
   Meme Webside https://memegen-link-examples-upleveled.netlify.app/ - webside to fetch the memes from
 */
 
-//idea: import node-fetch from package.json
+// idea: import node-fetch from package.json
 // const fs = require('fs');
 // const path = require('path');
 /* converted the lines above to the lines bellow to make them work (and compatible with
@@ -39,13 +40,13 @@ import { fileURLToPath } from 'url';
 // const fs = require('fs');
 // const path = require('path');
 const currentDir = dirname(fileURLToPath(import.meta.url));
-fs.mkdir(path.join(currentDir, 'memes'), (err) => {
+fs.mkdir(path.join(currentDir, 'memes'), { recursive: true }, (err) => {
   if (err) {
     return console.error(err);
   }
   console.log('Directory created successfully!');
-  // if not err don't show anything
 });
+// if not err don't show anything
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 // __dirname yellow line will disappear once I recomment the creation of the memes dir in
@@ -59,12 +60,24 @@ import getCookie from './util/cookies/getCookie.js';
 // const getCookie = require('./util/cookies/getCookie.js');
 */
 
-fetch('https://memegen-link-examples-upleveled.netlify.app/').then((response) =>
-  response.text(),
-); // Die Webside ist utner inspect html text
+const fetchedText = fetch(
+  'https://memegen-link-examples-upleveled.netlify.app/',
+).then((response) => response.text()); // Die Webside ist unter inspect html text
 // .then((data) => console.log(data));
+console.log(fetchedText);
+/*
+Without console.log:
+fetch('https://memegen-link-examples-upleveled.netlify.app/').then((response) =>
+   response.text(),
+ );
+With console.log:
+fetch('https://memegen-link-examples-upleveled.netlify.app/').then((response) =>
+  response.text().then((data) => console.log(data)),
+  );
 
-// fetch needs await before the code can continue - async await und anschließend library finden die mir den html string in etwas brauchbares umwandelt. Irgendwann muss das Ergebnis in einer Variable gespeichert werden. Braucche Cheerio um den String dess HTML-Textes den ich bekomme in etwas umzuwnadeln mit dem ich arbeiten kann.
+ */
+
+// fetch needs await before the code can continue - async await und anschließend library (cheerio) finden die mir den html string in etwas brauchbares umwandelt. Irgendwann muss das Ergebnis in einer Variable gespeichert werden. Braucche Cheerio um den String dess HTML-Textes den ich bekomme in etwas umzuwnadeln mit dem ich arbeiten kann.
 
 // const url = 'https://memegen-link-examples-upleveled.netlify.app.json';
 // async function download() {
@@ -72,6 +85,11 @@ fetch('https://memegen-link-examples-upleveled.netlify.app/').then((response) =>
 //   const buffer = await response.buffer();
 //   fs.writeFile(`./image.jpg`, buffer, () =>
 //     console.log('finished downloading!'),
-//   );
-// }
-console.log('test');
+// )};
+
+/* Doonerstagabend, 09.09.2021:
+- fixed error message appearing after memes dir has allready been created
+- fixed fetched html text not beeing shown in the console
+  -> makes me wonder if it this is necessary in the first place or is it sufficient to not be shown in the console?
+- created a backup dir in the projects dir containing a fall-back version of the index.js file
+*/
