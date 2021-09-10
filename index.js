aa5d02c6c6262257041854b790e1c07d7d6835dc
Philipp Binder <binder.philipp10@gmail.com>
2021-09-10 22:10:17 +0200

    Add comments

--- index.js
+++ index.js
@@ -15,52 +15,42 @@ https://www.geeksforgeeks.org/node-js-fs-mkdir-method/
 -----------------------  I M P O R T A N T  -----------------------
 installed dependancies:
   - Node Fetch https://www.npmjs.com/package/node-fetch - for fechting the images from the meme page
-  - ESM https://reactgo.com/node-es6-imports/ - for making Node Fetch work
+  - ESM https://reactgo.com/node-es6-imports/ - for making Node Fetch work - not used???
   - maybe instead https://www.npmjs.com/package/esm
   - Cheerio https://www.npmjs.com/package/cheerio
 
 miscellaneous:
   https://www.geeksforgeeks.org/node-js-fs-mkdir-method/ - shows me the code I need to write for dir on other PC's who use my file
   Meme Webside https://memegen-link-examples-upleveled.netlify.app/ - webside to fetch the memes from
+  Avoid __dirname not available error: https://stackoverflow.com/questions/46745014/alternative-for-dirname-in-node-when-using-the-experimental-modules-flag
 */
 
 import * as cheerio from 'cheerio';
 // or without "* as"?
 // idea: import node-fetch from package.json
-// const fs = require('fs');
-// const path = require('path');
-/* converted the lines above to the lines bellow to make them work (and compatible with
-import fs from 'fs';
+import fs from 'fs'; // part of node-fetch
 import fetch from 'node-fetch';
-*/
-import fs from 'fs';
-import fetch from 'node-fetch';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import path, { dirname } from 'path'; // gemäß Karl umgeschrieben, siehe Quelle unten
+import { fileURLToPath } from 'url'; // This line and the line above come from https://stackoverflow.com/questions/46745014/alternative-for-dirname-in-node-when-using-the-experimental-modules-flag
+
+// import { string } from 'prop-types'; - Ist automatisch kreiert worden im Rahmen der Anwendung Cheerios auf die Fetch-Request?
+
+// import instead of require because require causes errors
 
 // //idea: import node-fetch from package.json
 // const fs = require('fs');
 // const path = require('path');
-const currentDir = dirname(fileURLToPath(import.meta.url));
+const currentDir = dirname(fileURLToPath(import.meta.url)); // __dirname umgeschrieben
 fs.mkdir(path.join(currentDir, 'memes'), { recursive: true }, (err) => {
+  // __dirname umgeschrieben
   if (err) {
     return console.error(err);
   }
   console.log('Directory created successfully!');
 });
-// if not err don't show anything
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-// __dirname yellow line will disappear once I recomment the creation of the memes dir in
-
 // creates new dir ond users PC
-
-/*
-import getCookie from './util/cookies/getCookie.js';
-// const getCookie = require('./util/cookies/getCookie.js');
-
-// const getCookie = require('./util/cookies/getCookie.js');
-*/
 
 // const fetchedText = fetch(
 //   'https://memegen-link-examples-upleveled.netlify.app/',
@@ -70,7 +60,7 @@ import getCookie from './util/cookies/getCookie.js';
 // console.log(fetchedText);
 // Working fetch request and console log
 
-// fetches teh wabpage
+// fetches the wabpage
 const response = await fetch(
   'https://memegen-link-examples-upleveled.netlify.app/',
 );
@@ -80,35 +70,20 @@ const body = await response.text();
 // load the const body into cheerio
 const $ = cheerio.load(body);
 
-// console.log($.html()); = evidence that the comamnd above works
+// console.log($.html());
+// evidence that the comamnd above works
 
-$.html();
-// => <html><head></head><body><h2 class="title welcome">Hello there!</h2></body></html>
-
-// $(body);
-// $.root().html();
-// unnötig da ich nicht ide ganze Webside will?
-
-// console.log(body);
+// search for the sction with the id images (which contains all divs with imags) in the html of the webpage
+// Der untere Code sucht alle images raus die Kinder (also auch Enkelkinder) von #images (section im html text) sind und speichert sie unter der const images. Anschließend müssen diese in ein array umgewandelt werden damit ich anschließend die ersten 10 raussuchen kann.
+const images = $('#images img');
+// render  the document
+console.log($.html(images));
+// find string.trim() to transform the images into an array
+// for ( i= 0, i < 10, i++) {}
 
 /*
-Remove variable to avoid promise pending - but shouldn't I work around that?
-
-Without console.log:
-fetch('https://memegen-link-examples-upleveled.netlify.app/').then((response) =>
-   response.text(),
- );
-With console.log:
-fetch('https://memegen-link-examples-upleveled.netlify.app/').then((response) =>
-  response.text().then((data) => console.log(data)),
-  );
-
-
-
-
   ----------------------------------------------------------------------------------------------------------
  fetch needs await before the code can continue - async await und anschließend library (cheerio) finden die mir den html string in etwas brauchbares umwandelt. Irgendwann muss das Ergebnis in einer Variable gespeichert werden. Braucche Cheerio um den String dess HTML-Textes den ich bekomme in etwas umzuwnadeln mit dem ich arbeiten kann.
-
  */
 
 console.log('test');
