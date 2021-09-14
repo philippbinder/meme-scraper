7b1e4f89c172ef14e54e0b8d875035f9d0ec9190
Philipp Binder <binder.philipp10@gmail.com>
2021-09-14 14:59:58 +0200

    Change function to loop over links array

--- containerForComments.js
+++ containerForComments.js
@@ -1,4 +1,3 @@
-
 /*
 fetch('https://memegen-link-examples-upleveled.netlify.app/')
   .then((res) => res.blob)
@@ -65,4 +64,86 @@ const snake_case_var = 1;
 OtherComponent(PascalCaseVar, snake_case_var);
 
 OtherComponent(PascalCaseVar, snake_case_var);
+*/
+
+// ----------------------------------------- failed attempts ------------------------------------------------------
+
+// https.get(url, (res) => {
+//   // Image will be stored at this path
+//   const dirPath = `${__dirname}./memes/img.jpeg`;
+//   const filePath = fs.createWriteStream(dirPath);
+//   res.pipe(filePath);
+//   filePath.on('finish', () => {
+//     filePath.close();
+//     console.log('Download Completed');
+//   });
+// });
+
+// --------------------------------------------------------------------------------
+//  export const links2 = links;
+
+// const createFile = (filePath, fileContent) => {
+//   fs.writeFile('./memes', links2, (error) => {
+//     if (error) {
+//       console.error('An error occured: ', error);
+//     } else {
+//       console.log('Your file is made!');
+//     }
+//   });
+// };
+// https://levelup.gitconnected.com/use-node-js-to-to-create-directories-and-files-734063ce93ec
+// console.log(links2);
+// --------------------------------------------------------------
+
+// console.log(links);
+// export { links };
+// const loop = () => {
+//   for (let i = 1; i < arr.length; i++) {
+//     if (i === 10) {
+//       break;
+//     }
+//   }
+// };
+// console.log(loop);
+// Finally!!! Break out of loop based on array index src= "https://betterprogramming.pub/three-ways-to-exit-an-array-loop-before-completion-in-javascript-56cc6ffff820"
+
+// console.log(arr[i]);
+// if ([arr[i] === arr.length[11]]) { https://love2dev.com/blog/javascript-for-loop-foreach/
+// if ([i === 11]) {
+//   break;
+// }
+
+// const srcArray = arr.map((functionName) => {
+//   for (let i = 0; i < 10; i++) {
+//     // document.getElementsByTagName('src'); // doucment is not defined
+//   }
+// });
+
+// console.log(srcArray);
+
+// ------------------------------------------
+// const images3 = images2.trim();
+// console.log(images3);
+// const arr = images3.split(' '); // https://stackoverflow.com/questions/51010849/string-to-array-node-js-javascript
+// console.log(arr);
+
+// -----------------
+// const trimedImages = images2.trim();
+// console.log(trimedImages);
+// find string.trim() to transform the images into an array
+// for ( i= 0, i < 10, i++) {}
+
+// const url = 'https://memegen-link-examples-upleveled.netlify.app.json';
+// async function download() {
+//   const response = await fetch(url);
+//   const buffer = await response.buffer();
+//   fs.writeFile(`./image.jpg`, buffer, () =>
+//     console.log('finished downloading!'),
+// )};
+
+/* Donnerstagabend, 09.09.2021:
+- fixed error message appearing after memes dir has allready been created
+- fixed fetched html text not beeing shown in the console
+  -> makes me wonder if it this is necessary in the first place or is it sufficient to not be shown in the console?
+- created a backup dir in the projects dir containing a fall-back version of the index.js file
 */
